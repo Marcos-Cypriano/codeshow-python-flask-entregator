1f7af2a1a196315b012bf623b416b8f7921ab7d4
Marcos-Cypriano <ma_cypriano@hotmail.com>
2021-08-10 09:18:08 -0300

    Instalable app and added tests

---
+++ entregator/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+def read(filename):
+    return [req.strip() for req in open(filename).readlines()]
+
+setup(
+    name='entregator',
+    version='0.1.0', # major, minor, patch
+    description='App delivery',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read('requirements.txt'),
+    extras_require={
+        "dev": read('requirements-dev.txt')
+    }
+)
---
+++ entregator/tests/test_app.py
@@ -0,0 +1,9 @@
+def test_app_is_created(app):
+    assert app.name == 'entregator.app'
+
+def test_config_is_loaded(config):
+    assert config['DEBUG'] is False
+
+def test_request_returns_404(client):
+    assert client.get('/url_nao_existente').status_code == 404
+
