0197b697bb40cc8ec60f712c45ddd3cdf78ff940
Marcos-Cypriano <ma_cypriano@hotmail.com>
2021-08-12 13:59:14 -0300

    Started templates with bulma

--- entregator/entregator/app.py
+++ entregator/entregator/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from entregator.ext import site
+from entregator.ext import config
+from entregator.ext import toolbar
 
 def create_app():
     app = Flask(__name__)
+    config.init_app(app)    
+    toolbar.init_app(app)    
     site.init_app(app)    
     return app
--- entregator/entregator/ext/site/main.py
+++ entregator/entregator/ext/site/main.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template
 
 bp = Blueprint('site', __name__)
 
 @bp.route('/')
 def index():
-    return render_template(
-        'index.html', 
-        name=request.args['name']
-        )
+    return render_template('index.html')
+
+@bp.route('/sobre')
+def about():
+    return render_template('about.html')
+
+@bp.route('/restaurantes')
+def restaurants():
+    return render_template('restaurants.html')
